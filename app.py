57d1d6ab538bd8481ad778af4916c0f126b1a632
202402060028-web <202402060028@mitaoe.ac.in>
2025-11-15 02:08:07 +0530

    Refactor predict function to use TrafficData model

--- app.py
+++ app.py
@@ -13,12 +13,6 @@ scaler = joblib.load("scaler.pkl")
 def home():
     return {"message": "Traffic Congestion Model API Running"}
 
-@app.post("/predict")
-def predict(data: dict):
-    values = np.array(list(data.values())).reshape(1, -1)
-    values = scaler.transform(values)
-    prediction = model.predict(values)[0]
-    return {"prediction": int(prediction)}
 
 class TrafficData(BaseModel):
     time: str
@@ -29,4 +23,22 @@ class TrafficData(BaseModel):
     bus_count: float
     truck_count: float
     total: float
+
+@app.post("/predict")
+def predict(data: TrafficData):
+    values = np.array([[
+        data.time,
+        data.date,
+        data.day_of_week,
+        data.car_count,
+        data.bike_count,
+        data.bus_count,
+        data.truck_count,
+        data.total
+    ]], dtype=float)
+
+    values = scaler.transform(values)
+    prediction = model.predict(values)[0]
+    return {"prediction": int(prediction)}
+
 
